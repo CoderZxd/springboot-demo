e65c7765c493a3013b342a4186c41d2b9d335e47
Admin <zxd_0825@163.com>
2019-04-28 22:28:44 +0800

    add comment

--- src/main/java/com/zxd/test/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/zxd/test/springbootdemo/SpringbootDemoApplication.java
@@ -2,10 +2,16 @@ package com.zxd.test.springbootdemo;
 
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@SpringBootApplication   //same as @Configuration,@EnableAutoConfiguration,@ComponentScan
+//@Configuration
+//@EnableAutoConfiguration
+//@ComponentScan
 public class SpringbootDemoApplication {
 
 	public static void main(String[] args) {
